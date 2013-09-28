e58f3929e6c3b047e28c26174ef65b0b06c4a676
Brendan Bates <batesbrendan@gmail.com>
2013-09-29 00:19:49 +1000

    Change width of gauges based on scores

--- src/cljs/asylum/view.cljs
+++ src/cljs/asylum/view.cljs
@@ -6,58 +6,58 @@
 (def map-selector "#map")
 
 (defn- init-map 
-      "Initialise the map background that forms the basis for the asylum UI"
-      []
-      (.gmap3 
-        ($ map-selector)
-        (clj->js {
-                  :map (clj->js {
-                                 :address "Ghan, NT, Australia"
-                                 :options (clj->js {
-                                                    :color "#476DD5"
-                                                    :zoom 4
-                                                    :mapTypeId js/google.maps.MapTypeId.ROADMAP,
-                                                    :scrollwheel false
-                                                    :center "-25.085875,134.284057"
-                                                    :draggable false
-                                                    :disableDefaultUI true
-                                                    :disableDoubleClickZoom true
-                                                    :styles 
-                                                    [
-                                                     (clj->js {
-                                                               :featureType "administrative.province"
-                                                               :elementType "labels.text"
-                                                               :stylers [(clj->js { :visibility "off" })]
-                                                               }),
-                                                     (clj->js {
-                                                               :featureType "administrative.locality"
-                                                               :elementType "labels"
-                                                               :stylers [(clj->js { :visibility "off" })]
-                                                               }),
-                                                     (clj->js {
-                                                               :featureType "administrative.province"
-                                                               :elementType "geometry"
-                                                               :stylers [(clj->js { :visibility "off" })]
-                                                               }),
-                                                     (clj->js {
-                                                               :featureType "water"
-                                                               :elementType "labels.text"
-                                                               :stylers [(clj->js { :visibility "off" })]
-                                                               }),
-                                                     (clj->js {
-                                                               :featureType "administrative.country"
-                                                               :elementType "labels"
-                                                               :stylers [(clj->js { :visibility "off" })]
-                                                               }),
-                                                     (clj->js {
-                                                               :featureType "administrative.country"
-                                                               :elementType "geometry"
-                                                               :stylers [(clj->js { :visibility "off" })]
-                                                               }),
-                                                     (clj->js {
-                                                               :featureType "landscape.natural.landcover"
-                                                               :elementType "geometry"
-                                                               :stylers [(clj->js { :visibility "simplified" })]})]})})})))
+       "Initialise the map background that forms the basis for the asylum UI"
+       []
+       (.gmap3 
+         ($ map-selector)
+         (clj->js {
+                   :map (clj->js {
+                                  :address "Ghan, NT, Australia"
+                                  :options (clj->js {
+                                                     :color "#476DD5"
+                                                     :zoom 4
+                                                     :mapTypeId js/google.maps.MapTypeId.ROADMAP,
+                                                     :scrollwheel false
+                                                     :center "-25.085875,134.284057"
+                                                     :draggable false
+                                                     :disableDefaultUI true
+                                                     :disableDoubleClickZoom true
+                                                     :styles 
+                                                     [
+                                                      (clj->js {
+                                                                :featureType "administrative.province"
+                                                                :elementType "labels.text"
+                                                                :stylers [(clj->js { :visibility "off" })]
+                                                                }),
+                                                      (clj->js {
+                                                                :featureType "administrative.locality"
+                                                                :elementType "labels"
+                                                                :stylers [(clj->js { :visibility "off" })]
+                                                                }),
+                                                      (clj->js {
+                                                                :featureType "administrative.province"
+                                                                :elementType "geometry"
+                                                                :stylers [(clj->js { :visibility "off" })]
+                                                                }),
+                                                      (clj->js {
+                                                                :featureType "water"
+                                                                :elementType "labels.text"
+                                                                :stylers [(clj->js { :visibility "off" })]
+                                                                }),
+                                                      (clj->js {
+                                                                :featureType "administrative.country"
+                                                                :elementType "labels"
+                                                                :stylers [(clj->js { :visibility "off" })]
+                                                                }),
+                                                      (clj->js {
+                                                                :featureType "administrative.country"
+                                                                :elementType "geometry"
+                                                                :stylers [(clj->js { :visibility "off" })]
+                                                                }),
+                                                      (clj->js {
+                                                                :featureType "landscape.natural.landcover"
+                                                                :elementType "geometry"
+                                                                :stylers [(clj->js { :visibility "simplified" })]})]})})})))
 
 (def possible-boat-coords
      [[-11.178402 111.870115]
@@ -88,40 +88,43 @@
 (def boat-marker-opts {:options {:icon "img/boatPin.png"}})
 
 (defn- show-boats
-      "Given a number of boats to display, randomly place them on a journey to australia"
-      [num-boats]
-      (let [boats-coords (take num-boats (shuffle possible-boat-coords))
-            boats (map #(clj->js (merge {:latLng %} boat-marker-opts)) boats-coords)]
-           (.gmap3 
-             ($ map-selector)
-             
-             (clj->js {
-                       :clear (clj->js {:name ["marker"]})
-                       :marker (clj->js {:values boats})}))))
+       "Given a number of boats to display, randomly place them on a journey to australia"
+       [num-boats]
+       (let [boats-coords (take num-boats (shuffle possible-boat-coords))
+             boats (map #(clj->js (merge {:latLng %} boat-marker-opts)) boats-coords)]
+            (.gmap3 
+              ($ map-selector)
+              
+              (clj->js {
+                        :clear (clj->js {:name ["marker"]})
+                        :marker (clj->js {:values boats})}))))
 
 
 (defn- say
-      ([something]
-       (say "greeting" something))
-      ([id something]
-       (.text ($ (str "#" id)) something)))
+       ([something]
+        (say "greeting" something))
+       ([id something]
+        (.text ($ (str "#" id)) something)))
 
 (defn- update-levers
-      [state]
-      (let [levers (:levers state)]
-           (do 
-             (.val ($ "#offshore-intake") (:offshore-intake levers))
-             (.val ($ "#detention-proportion") (:detention-proportion levers)))))
+       [state]
+       (let [levers (:levers state)]
+            (do 
+              (.val ($ "#offshore-intake") (:offshore-intake levers))
+              (.val ($ "#detention-proportion") (:detention-proportion levers)))))
 
 (defn- update-gauges 
        [{popularity :popularity deaths :deaths}]
-       (-> ($ ".popularityValue") (.text (str (int (* 100 popularity)) "%")))
-       (-> ($ ".deathsValue") (.text (str (int (* 100 deaths)) "%"))))
+       (let [popularity (str (int (* 100 popularity)) "%")
+             deaths (str (int (* 100 deaths)) "%")]
+            (-> ($ ".popularityValue") (.text popularity) (.css "width" popularity))
+            (-> ($ ".deathsValue") (.text deaths) (.css "width" popularity))))
+
 
 (defn- option-colour 
-     [{morrison-index :morrison}]
-     (log morrison-index)
-     (if (> morrison-index 1) "orange" "blue")) 
+       [{morrison-index :morrison}]
+       (log morrison-index)
+       (if (> morrison-index 1) "orange" "blue")) 
 
 (defmulti on-event-choice-selection key)
 
@@ -137,18 +140,18 @@
        (let [morrison-index (effect {:morrison 1} 1)
              button-colour (option-colour morrison-index)]
             (-> ($ "<button>")
-	         (.addClass "button")
-	         (.addClass button-colour)
-	         (.text title)
-	         (.on "click" (comp click-fn (partial on-event-choice-selection option))))))
+              (.addClass "button")
+              (.addClass button-colour)
+              (.text title)
+              (.on "click" (comp click-fn (partial on-event-choice-selection option))))))
 
 (defn- show-event 
-      [{:keys [title content options]} apply-event-choice-fn]
-      (let [option-buttons (to-array (map (partial option-button apply-event-choice-fn) options))
-            content-div ($ "#event-panel")]           
-           (-> content-div (.find "header h2") (.text title))
-           (-> content-div (.find "section") (.html content))
-           (-> content-div (.find "footer") (.empty) (.append option-buttons))))
+       [{:keys [title content options]} apply-event-choice-fn]
+       (let [option-buttons (to-array (map (partial option-button apply-event-choice-fn) options))
+             content-div ($ "#event-panel")]           
+            (-> content-div (.find "header h2") (.text title))
+            (-> content-div (.find "section") (.html content))
+            (-> content-div (.find "footer") (.empty) (.append option-buttons))))
 
 (defn display [state apply-event-choice-fn]
       (do 
@@ -160,9 +163,9 @@
 
 
 (defn- lever-values 
-      "Collect the lever values"
-      []
-      {:levers {:offshore-intake (-> (.val ($ "#offshore-intake")) js/parseInt)
-                :detention-proportion (-> (.val ($ "#detention-proportion"))  js/parseFloat)}})
+       "Collect the lever values"
+       []
+       {:levers {:offshore-intake (-> (.val ($ "#offshore-intake")) js/parseInt)
+                 :detention-proportion (-> (.val ($ "#detention-proportion"))  js/parseFloat)}})
 
 ($ init-map)
