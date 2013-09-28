9dba27c74e25a6eb48e88806498d18e80413a817
Yair Iny <yair.iny@gmail.com>
2013-09-28 14:53:30 +1000

    events selection

--- src/cljs/asylum/events.cljs
+++ src/cljs/asylum/events.cljs
@@ -1,20 +1,20 @@
 (ns asylum.events)
 
 (def dummy-event
-  {:constraints [:morrison [0 1]
-                 :turn [1 6]]}
-  {:effect (fn [state] (update-in state [:population] dec))})
+  {:constraints {:morrison [0 1]
+                 :turn [1 6]}
+   :effect (fn [state] (update-in state [:population] dec))})
 
 (def event-store
-  dummy-event)
+  [dummy-event])
 
 (defn in-range [n [x y]]
   (<= x n y))
 
 (defn choose-event [state]
-  [let [{:keys [morisson turn]} state]
-   (rand-nth (filter (fn [evt] true) event-store))])
+  (let [{:keys [morisson turn]} state]
+    (rand-nth (filter (fn [evt] true) event-store))))
 
 (defn apply-event [state]
-  [let evt (choose-event state)]
-  state)
+  (let [evt (choose-event state)]
+    state))
