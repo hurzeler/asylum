99c30039abbb37121f66912fe108c15bd47c646d
Bernhard Hurzeler <hurzeler@icm-consulting.com.au>
2013-09-28 17:32:04 +1000

    added int-ovenant-breach

--- src/cljs/asylum/event_content.cljs
+++ src/cljs/asylum/event_content.cljs
@@ -6,3 +6,14 @@
                  :turn [1 6]}
    :effectss 1
    :effect (ee/dsr-factor :population -0.2)})
+
+(def mou-naru
+  {:title "Australian Government signs a new memorandum of understanding with Nauru"
+   :media {:name mou-naru.jpg :type image}
+   :content "The Australian Government signs a new memorandum of understanding with Nauru similar to its Regional Resettlement Arrangement with Papua New Guinea. Asylum seekers who are transferred to Nauru for processing and found to be refugees could be settled in Nauru permanently."
+   :event-date "3 Aug 2013"
+   :link "http://www.sbs.com.au/news/article/2013/08/04/australia-signs-asylum-deal-nauru"
+   :constraints {:morrison [0 1]
+                 :turn [1 6]}
+   :effectss 1
+   :effect (ee/dsr-factor :population -0.2)})
