3ee1fd2a0d0f95aa72b498e6d18decc8c67fa313
Yair Iny <yair.iny@gmail.com>
2013-09-29 22:26:28 +1000

    about us

--- src/cljs/asylum/about_us_content.cljs
+++ src/cljs/asylum/about_us_content.cljs
@@ -3,6 +3,6 @@
 (def about-us
   {:title "About Us"
    :media {:name "about-us.jpg" :type :image}
-   :content "<p>We are team of software engineers that have been working together at ICM Consulting Pty Ltd in Sydney Australia. Over the years we have developed many enterprise applications for some of the largest companies in Australia in the financial, telecomunications and logistics industries. We have delivered solutions for the private as well as  government clients federal as well as state</p><p>Some of our clients include the NBN Co, Telstra, Macquarie Bank, APRA, NSW Health and many others</p><p>we have developed many interesting products in the mobile sector. Look at <a href=\"http://www.icm-consulting.com.au\">ICM Consulting Pty Ltd Products</a></p><p>Contact us</p><p>Level 17, Suite 1702<br/>109 Pitt Street<br/>Sydney NSW 2000, Australia</p><p>+61 2 9028 7081</p><p><a href=\"mailto:sales@icm-consulting.com.au?Subject=ASYLUM - Clojure Cup\">Send Mail</a></p>"
-   :links ["http://www.icm-consulting.com.au"]
+   :content "<p>We are team of software engineers that work together in Sydney, Australia. We started using Clojure a few years ago and find it really useful for solving almost any problem.  We decided to make this app to raise awareness to this issue, and hope this has been a learning experience.  We're very happy to be part of the inaugural Clojure Cup event.  If you'd like to vote for us, please do so <a href='http://clojurecup.com/app.html?app=asylum'>here</a>.  If you'd like to get in touch, please email us at <a href='mailto:info@icm-consulting.com.au'>info@icm-consulting.com.au</a>.</p>"
+   :links []
    :options {:dismiss {:effect (fn [] {}) :title "Thanks, now I know" :description ""}}})
