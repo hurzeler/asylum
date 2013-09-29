652338d78619aea97addddbabfbd5b5dd7bee08a
Yair Iny <yair.iny@gmail.com>
2013-09-29 18:45:53 +1000

    modified event text

--- src/cljs/asylum/event_content.cljs
+++ src/cljs/asylum/event_content.cljs
@@ -10,8 +10,8 @@
    :constraints {:morrison [0 1]
                  :turn [1 100]}
     :options
-   {:good {:effect (ee/dsr-factor :morrison -0.02) :title "No action" :description ""}
-    :bad {:effect (ee/dsr-factor :morrison 0.01) :title "Review" :description "Review being a signatory to UN convention"}}})
+   {:good {:effect (ee/dsr-factor :morrison -0.02) :title "Decline" :description ""}
+    :bad {:effect (ee/dsr-factor :morrison 0.01) :title "Sign MOU" :description "Sing the MOU"}}})
 
 (def int-covenant-breach
   {:title "Australia has breached the International Covenant on Civil and Political Rights by indefinitely detaining refugees who have failed security assessments"
@@ -40,25 +40,25 @@
 (def png-resettlement
   {:title "Regional Resettlement Arrangement with Papua New Guinea"
    :media {:name "resettlement-agreement.jpg" :type :image}
-   :content "<p>Prime Minister considers a new Regional Resettlement Arrangement with Papua New Guinea, whereby all asylum seekers arriving in Australia by boat will be transferred to Papua New Guinea for processing and, if they are found to be refugees, permanent settlement. They would not have the opportunity to seek asylum or settle in Australia.</p>"
+   :content "<p>Australia considers a new Regional Resettlement Arrangement with Papua New Guinea, whereby all asylum seekers arriving in Australia by boat will be transferred to Papua New Guinea for processing and, if they are found to be refugees, permanent settlement. They would not have the opportunity to seek asylum or settle in Australia.</p>"
    :event-date "19 July 2013"
    :links ["http://unhcr.org.au/unhcr/index.php?option=com_content&view=article&id=344:unhcr-australia-papua-new-guinea-asylum-agreement-presents-protection-challenges-&catid=35:news-a-media&Itemid=63"] 
    :constraints {:morrison [0.2 1]
                  :turn [4 100]}
     :options
-   {:good {:effect (ee/dsr-factor :morrison -0.02) :title "No action" :description ""}
+   {:good {:effect (ee/dsr-factor :morrison -0.02) :title "Decline" :description ""}
     :bad {:effect (ee/dsr-factor :morrison 0.05) :title "Sign agreement" :description "All asylum seekers will be settled in PNG"}}})
 
 (def unchr-report-manus-island
   {:title "UNHCR report on Manus Island"
    :media {:name "manus-island.jpg" :type :image}
-   :content "<p>A second UNHCR report on conditions in the offshore processing facility on Manus Island acknowledges that, while there have been some positive developments, conditions remain below international standards for the reception and treatment of asylum-seekers . In particular, UNHCR expresses ongoing concern about the harsh physical living conditions, the mandatory and indefinite detention of asylum seekers, the lack of adequate safeguards in transfer arrangements and shortcomings in the legal framework for refugee status determination.</p>"
+   :content "<p>A UNHCR report on conditions in the offshore processing facility on Manus Island acknowledges that, while there have been some positive developments, conditions remain below international standards for the reception and treatment of asylum-seekers . In particular, UNHCR expresses ongoing concern about the harsh physical living conditions, the mandatory and indefinite detention of asylum seekers, the lack of adequate safeguards in transfer arrangements and shortcomings in the legal framework for refugee status determination.</p>"
    :event-date "12 July 2013"
    :links ["http://unhcr.org.au/unhcr/files/2013-07-12_Manus_Island_Report_Final%281%29.pdf"] 
    :constraints {:morrison [0.4 1]
                  :turn [4 100]}
     :options
-   {:good {:effect (ee/dsr-factor :morrison -0.02) :title "Review" :description "Order a review of offshore facilities"}
+   {:good {:effect (ee/dsr-factor :morrison -0.02) :title "Accept Report" :description "Order a review of offshore facilities"}
     :bad {:effect (ee/dsr-factor :morrison 0.05) :title "Ignore Report" :description "You consider the Australian government not being a signatury of the UNHCR convention"}}})
 
 (def resumption-processing-asylum-claims
@@ -99,7 +99,7 @@
 (def redcross-inaugural-vulnerability-report
   {:title "Red Cross Inaugural Vulnerability Report"
    :media {:name "redcross.png" :type :image}
-   :content "<p>The Red Cross releases its inaugural Vulnerability Report, focusing on asylum seekers living in the Australian community under the no advantage  test. The Report reveals that many people seeking asylum live in the Australian community in a state of poverty, with many struggling to access adequate and sustainable housing and afford basics such as food, furniture and bedding. In launching the report, Red Cross CEO Robert Tickner says \"We are not talking about destitution here by normal standards. We're talking about what is by any measure among the most destitute people in the country, who are absolutely marginalised by every reasonable measure.\"</p>"
+   :content "<p>The Red Cross releases its Inaugural Vulnerability Report, focusing on asylum seekers living in the Australian community under the no advantage  test. The report reveals that many people seeking asylum live in the Australian community in a state of poverty, with many struggling to access adequate and sustainable housing and afford basics such as food, furniture and bedding. In launching the report, Red Cross CEO Robert Tickner says \"We are not talking about destitution here by normal standards. We're talking about what is by any measure among the most destitute people in the country, who are absolutely marginalised by every reasonable measure.\"</p>"
    :event-date "June 2013"
    :links ["http://www.redcross.org.au/files/20130614_Inaugural_Vulnerability_Report_LR.pdf"] 
    :constraints {:morrison [0.3 1]
@@ -118,7 +118,7 @@
                  :turn [1 100]}
     :options
    {:good {:effect (ee/dsr-factor :morrison -0.03) :title "Reject" :description ""}
-    :bad {:effect (ee/dsr-factor :morrison 0.05) :title "Pass" :description "Pass excission policy legislation"}}})
+    :bad {:effect (ee/dsr-factor :morrison 0.05) :title "Enact" :description "Pass excission policy legislation"}}})
 
 (def full-federal-court-challange
   {:title "Full Federal Court finds that the International Treaty Obligations Assessment process is unlawful"
@@ -133,9 +133,9 @@
     :bad {:effect (ee/dsr-factor :morrison 0.05) :title "Appeal" :description "You fight the courts decision"}}})
 
 (def resettlemet-deal-new-zealand
-  {:title "Resettlemet deal with New Zealand"
+  {:title "Resettlement deal with New Zealand"
    :media {:name "nz-refugees.jpg" :type :image}
-   :content "<p>The Australian Government enter a new resettlemet deal with New Zealand, whereby 150 places from New Zealand's overall resettlement quota of 750 will be allocated to refugees who sought asylum in Australia. The refugees may be resettled directly from Australia or from offshore processing centres in Nauru and Manus Island.</p>"
+   :content "<p>The Australian Government considers a new resettlement deal with New Zealand, whereby 150 places from New Zealand's overall resettlement quota of 750 will be allocated to refugees who sought asylum in Australia. The refugees may be resettled directly from Australia or from offshore processing centres in Nauru and Manus Island.</p>"
    :event-date "9 February 2013"
    :links ["http://www.immigration.govt.nz/migrant/general/generalinformation/media/refugeefactsheet.htm"] 
    :constraints {:morrison [0 1]
@@ -159,7 +159,7 @@
 (def unhcr-nauru-report
   {:title "UNHCR Nauru condition report"
    :media {:name "nauru.jpg" :type :image}
-   :content "<p>UNHCR releases a report on conditions in the offshore processing facility in Nauru. It concludes that the transfer of asylum-seekers to what are currently harsh and unsatisfactory temporary facilities, with in a closed detention setting, and in the absence of a fully functional legal framework and adequately capacitated system to assess refugee claims, do not currently meet the required protection standards. </p>"
+   :content "<p>UNHCR releases a report on conditions in the offshore processing facility in Nauru. It concludes that the transfer of asylum-seekers to what are currently harsh and unsatisfactory temporary facilities, within a closed detention setting, and in the absence of a fully functional legal framework and adequately capacitated system to assess refugee claims, do not currently meet the required protection standards. </p>"
    :event-date "14 December 2012"
    :links ["http://unhcr.org.au/unhcr/images/Amended%20footnote%202012-12-14%20nauru%20monitoring%20report%20final_2.pdf"] 
    :constraints {:morrison [0.2 1]
@@ -169,9 +169,9 @@
     :bad {:effect (ee/dsr-factor :morrison 0.07) :title "Ignore" :description "You choose to ignore the UNHCR report"}}})
 
 (def no-advantage-test
-  {:title "no advantage  test"
+  {:title "No Advantage Test"
    :media {:name "no-advantage.jpg" :type :image}
-   :content "<p>Following a significant increase in boat arrivals, the Australian Government considers not to transfer all asylum seekers who arrived by boat after today to offshore processing centres in the immediate future. As a result, some of these asylum seekers will be processed in Australia, but will still be subject to the no advantage  test. This would mean that:
+   :content "<p>Following a significant increase in boat arrivals, the Australian Government considers not to transfer all asylum seekers who arrived by boat after today to offshore processing centres in the immediate future. As a result, some of these asylum seekers will be processed in Australia, but will still be subject to the no advantage test. This would mean that:
    They will be released into the community on bridging visas but will not have the right to work.
    If they are found to be refugees, they will remain on bridging visas and will not receive a permanent visa until they have waited  for the same length of time that they would have waited, if they had applied for resettlement overseas.
    They may be transferred to Nauru or Manus Island at any time.</p>"
@@ -180,8 +180,8 @@
    :constraints {:morrison [0.2 1]
                  :turn [2 100]}
     :options
-   {:good {:effect (ee/dsr-factor :morrison -0.03) :title "Take no action" :description ""}
-    :bad {:effect (ee/dsr-factor :morrison 0.07) :title "Introduce no-advantage test" :description "You accept that a no-advantage test is necessary"}}})
+   {:good {:effect (ee/dsr-factor :morrison -0.03) :title "Decline" :description ""}
+    :bad {:effect (ee/dsr-factor :morrison 0.07) :title "Introduce Test" :description "You accept that a no-advantage test is necessary"}}})
 
 (def offshore-processing-png
   {:title "Offshore processing in PNG"
@@ -192,7 +192,7 @@
    :constraints {:morrison [0 1]
                  :turn [1 100]}
     :options
-   {:good {:effect (ee/dsr-factor :morrison -0.02) :title "Take no action" :description ""}
+   {:good {:effect (ee/dsr-factor :morrison -0.02) :title "Decline" :description ""}
     :bad {:effect (ee/dsr-factor :morrison 0.05) :title "Implement" :description "Start prossesing refugees in PNG"}}})
 
 (def enhanced-screening
@@ -232,7 +232,7 @@ Refugees who arrived by boat before today (except for unaccompanied minors) will
    :constraints {:morrison [0 1]
                  :turn [1 100]}
     :options
-   {:good {:effect (ee/dsr-factor :morrison -0.02) :title "Reject" :description "You are not in favour of offshore processing"}
+   {:good {:effect (ee/dsr-factor :morrison -0.02) :title "Decline" :description "You are not in favour of offshore processing"}
     :bad {:effect (ee/dsr-factor :morrison 0.05) :title "Implement" :description ""}}})
 
 (def increase-intake-20000
@@ -287,7 +287,7 @@ Reintroducing offshore processing of asylum seekers in Nauru and Papua New Guine
                  :turn [1 100]}
     :options
    {:good {:effect (ee/dsr-factor :morrison 0.05) :title "Implement" :description "You appoint an expert panel"}
-    :bad {:effect (ee/dsr-factor :morrison -0.05) :title "Take no action" :description "You reconsider and will not appoint an expert panel"}}})
+    :bad {:effect (ee/dsr-factor :morrison -0.05) :title "Decline" :description "You reconsider and will not appoint an expert panel"}}})
 
 (def joint-select-committee
   {:title "Joint Select Committee's findings"
